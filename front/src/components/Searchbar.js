11abdfc7e3544fdb206c3ad248669e60c6a6c200
lasyapriya <csedm9@gmail.com>
2025-12-09 10:32:42 +0530

    Update Searchbar.js

--- front/src/components/Searchbar.js
+++ front/src/components/Searchbar.js
@@ -1,1 +1,17 @@
-n
+import React from 'react';
+
+function SearchBar({ value, onChange }) {
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Search by customer name or phone..."
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        style={{ width: '100%', padding: '10px', marginBottom: '20px' }}
+      />
+    </div>
+  );
+}
+
+export default SearchBar;
