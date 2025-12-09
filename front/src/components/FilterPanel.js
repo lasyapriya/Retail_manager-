fe1233db03457f09dd8e179be1edb1cbf8e84af9
lasyapriya <csedm9@gmail.com>
2025-12-09 20:47:46 +0530

    Update FilterPanel.js

--- front/src/components/FilterPanel.js
+++ front/src/components/FilterPanel.js
@@ -4,68 +4,123 @@ import { useSpring, animated } from '@react-spring/web';
 function FilterPanel({ options, filters, setFilters }) {
   const handleMultiChange = (e, key) => {
     const selected = Array.from(e.target.options)
-      .filter(option => option.selected)
-      .map(option => option.value);
+      .filter((option) => option.selected)
+      .map((option) => option.value);
     setFilters({ ...filters, [key]: selected });
   };
 
-  // Spring for filter bounce on change
   const springProps = useSpring({
-    transform: 'scale(1)',
-    from: { transform: 'scale(0.95)' },
-    reset: true,
-    config: { tension: 400, friction: 10 },
+    from: { opacity: 0, transform: 'translateX(-10px)' },
+    to: { opacity: 1, transform: 'translateX(0px)' },
+    config: { tension: 280, friction: 20 },
   });
 
   return (
     <animated.div className="filter-panel" style={springProps}>
       <h3>Filters</h3>
-      <div>
+
+      <div className="filter-group">
         <label>Customer Region</label>
-        <select multiple value={filters.customerRegions} onChange={(e) => handleMultiChange(e, 'customerRegions')}>
-          {options.customerRegions?.map((r) => <option key={r} value={r}>{r}</option>)}
+        <select
+          multiple
+          value={filters.customerRegions}
+          onChange={(e) => handleMultiChange(e, 'customerRegions')}
+        >
+          {options.customerRegions?.map((r) => (
+            <option key={r} value={r}>
+              {r}
+            </option>
+          ))}
         </select>
       </div>
-      <div>
+
+      <div className="filter-group">
         <label>Gender</label>
-        <select multiple value={filters.genders} onChange={(e) => handleMultiChange(e, 'genders')}>
-          {options.genders?.map((g) => <option key={g} value={g}>{g}</option>)}
+        <select
+          multiple
+          value={filters.genders}
+          onChange={(e) => handleMultiChange(e, 'genders')}
+        >
+          {options.genders?.map((g) => (
+            <option key={g} value={g}>
+              {g}
+            </option>
+          ))}
         </select>
       </div>
-      <div>
+
+      <div className="filter-group">
         <label>Product Category</label>
-        <select multiple value={filters.productCategories} onChange={(e) => handleMultiChange(e, 'productCategories')}>
-          {options.productCategories?.map((c) => <option key={c} value={c}>{c}</option>)}
+        <select
+          multiple
+          value={filters.productCategories}
+          onChange={(e) => handleMultiChange(e, 'productCategories')}
+        >
+          {options.productCategories?.map((c) => (
+            <option key={c} value={c}>
+              {c}
+            </option>
+          ))}
         </select>
       </div>
-      <div>
+
+      <div className="filter-group">
         <label>Tags</label>
-        <select multiple value={filters.tags} onChange={(e) => handleMultiChange(e, 'tags')}>
-          {options.tags?.map((t) => <option key={t} value={t}>{t}</option>)}
+        <select
+          multiple
+          value={filters.tags}
+          onChange={(e) => handleMultiChange(e, 'tags')}
+        >
+          {options.tags?.map((t) => (
+            <option key={t} value={t}>
+              {t}
+            </option>
+          ))}
         </select>
       </div>
-      <div>
+
+      <div className="filter-group">
         <label>Payment Method</label>
-        <select multiple value={filters.paymentMethods} onChange={(e) => handleMultiChange(e, 'paymentMethods')}>
-          {options.paymentMethods?.map((m) => <option key={m} value={m}>{m}</option>)}
+        <select
+          multiple
+          value={filters.paymentMethods}
+          onChange={(e) => handleMultiChange(e, 'paymentMethods')}
+        >
+          {options.paymentMethods?.map((m) => (
+            <option key={m} value={m}>
+              {m}
+            </option>
+          ))}
         </select>
       </div>
-      <div>
+
+      <div className="filter-group">
         <label>Age Range</label>
         <input
           type="number"
           placeholder="Min"
           value={filters.ageMin}
-          onChange={(e) => setFilters({ ...filters, ageMin: e.target.value ? parseInt(e.target.value) : '' })}
+          onChange={(e) =>
+            setFilters({
+              ...filters,
+              ageMin: e.target.value ? parseInt(e.target.value, 10) : '',
+            })
+          }
         />
         <input
           type="number"
           placeholder="Max"
           value={filters.ageMax}
-          onChange={(e) => setFilters({ ...filters, ageMax: e.target.value ? parseInt(e.target.value) : '' })}
+          onChange={(e) =>
+            setFilters({
+              ...filters,
+              ageMax: e.target.value ? parseInt(e.target.value, 10) : '',
+            })
+          }
         />
       </div>
-      <div>
+
+      <div className="filter-group">
         <label>Date Range</label>
         <input
           type="date"
