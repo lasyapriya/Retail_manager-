6f3cd24d72a47bbc43a0471cf8a66c00ca71d0e6
lasyapriya <csedm9@gmail.com>
2025-12-09 10:33:31 +0530

    Update SortingDropdown.js

--- front/src/components/SortingDropdown.js
+++ front/src/components/SortingDropdown.js
@@ -1,1 +1,17 @@
+import React from 'react';
 
+function SortingDropdown({ value, onChange }) {
+  return (
+    <div className="sorting-dropdown">
+      <label>Sort by: </label>
+      <select value={value} onChange={(e) => onChange(e.target.value)}>
+        <option value="">None</option>
+        <option value="date_newest">Date (Newest First)</option>
+        <option value="quantity">Quantity (High to Low)</option>
+        <option value="customer_name_az">Customer Name (A-Z)</option>
+      </select>
+    </div>
+  );
+}
+
+export default SortingDropdown;
