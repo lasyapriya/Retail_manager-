ed463bb9fcd2037fe0625d0ab89424399fb67b23
lasyapriya <csedm9@gmail.com>
2025-12-09 19:36:19 +0530

    Update SearchBar.js

--- front/src/components/SearchBar.js
+++ front/src/components/SearchBar.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import { useSpring, animated } from '@react-spring/web';
 
 function SearchBar({ value, onChange }) {
+  const [isFocused, setIsFocused] = React.useState(false);
+
+  const springProps = useSpring({
+    width: isFocused ? '100%' : '80%', // Elastic stretch
+    config: { mass: 1, tension: 300, friction: 15 }, // Bouncy
+  });
+
   return (
-    <div className="search-bar">
+    <animated.div className="search-bar" style={springProps}>
       <input
         type="text"
         placeholder="Search by customer name or phone..."
         value={value}
         onChange={(e) => onChange(e.target.value)}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
         style={{ width: '100%', padding: '10px', marginBottom: '20px' }}
       />
-    </div>
+    </animated.div>
   );
 }
 
