b6fbddeae7ec99745e01d54957704b488e9a0407
lasyapriya <csedm9@gmail.com>
2025-12-09 10:34:18 +0530

    Update Pagination.js

--- front/src/components/Pagination.js
+++ front/src/components/Pagination.js
@@ -1,1 +1,13 @@
+import React from 'react';
 
+function PaginationControls({ page, setPage, pages, total }) {
+  return (
+    <div className="pagination">
+      <button disabled={page === 1} onClick={() => setPage(page - 1)}>Previous</button>
+      <span>Page {page} of {pages} ({total} total)</span>
+      <button disabled={page >= pages} onClick={() => setPage(page + 1)}>Next</button>
+    </div>
+  );
+}
+
+export default PaginationControls;
