dffdf5ba7d4c502c3da00c34fb2d62237c744f96
lasyapriya <csedm9@gmail.com>
2025-12-09 16:13:04 +0530

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './styles/App.css';
+import './App.css';
 import SearchBar from './components/SearchBar';
 import FilterPanel from './components/FilterPanel';
 import SortingDropdown from './components/SortingDropdown';
